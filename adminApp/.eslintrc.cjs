70831e55030a9de412c403604706f2afb8dd8b52
Н С <nikolai3578@gmail.com>
2023-02-07 03:51:37 +0300

    added eslint

---
+++ adminApp/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'no-console': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'import/no-cycle': 0,
+    'import/prefer-default-export': 0,
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': 0,
+  },
+}
