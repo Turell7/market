5013e152e83021f2d8d4422a2af5ec28a74a00e5
Turell7 <65603111+Turell7@users.noreply.github.com>
2023-02-12 23:59:23 +0300

    Revert "add footer minimum"

--- admin-app/src/layouts/Footer/index.jsx
+++ admin-app/src/layouts/Footer/index.jsx
@@ -1,12 +1,7 @@
-import { Box, Typography } from '@mui/material'
-import './footer.css'
-
 export function Footer() {
   return (
-    <Box className="footer">
-      <Typography variant="h6">
-        © 2023 The best dev team
-      </Typography>
-    </Box>
+    <footer>
+      Здесь будет Footer
+    </footer>
   )
 }
