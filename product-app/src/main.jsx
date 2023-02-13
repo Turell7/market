9fb17e1168c950641297a06689ceca57aca90bf1
Irina Gorshkova <komlyanovna174@mail.ru>
2023-02-13 17:18:07 +0500

    added header component and its initial layout

--- product-app/src/main.jsx
+++ product-app/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
+import { Contacts } from './components/Contacts/Contacts'
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/contacts",
+        element: <Contacts />
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
