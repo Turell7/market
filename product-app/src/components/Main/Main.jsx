9ace4127e9dcadf2c1d358bac9f831eecf717ccf
Irina Gorshkova <komlyanovna174@mail.ru>
2023-02-14 10:57:04 +0500

    Added categories and sticky footer

--- product-app/src/components/Main/Main.jsx
+++ product-app/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import { TopNav } from '../TopNav/TopNav'
 import style from './styles.module.css'
 
 export function Main() {
   return (
     <>
       <main className={style.containerMain}>
-        Main
+        <TopNav />
         <Outlet />
+        Main
       </main>
     </>
   )
