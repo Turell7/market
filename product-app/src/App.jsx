2d60e38d0ed65a8e22e4a5165a7ce2cee44c4745
Irina Gorshkova <komlyanovna174@mail.ru>
2023-02-17 15:26:03 +0500

    registration and authorization requests. Added redux-toolkit

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -3,19 +3,34 @@ import './App.css'
 import { Footer } from './layouts/Footer'
 import { Header } from './layouts/Header'
 import { Main} from './layouts/Main'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
 
   return (
-    <div className="App">
-    <Header />
-      <Main>
-        <div>
-          <Outlet />
-        </div>
-      </Main>
-    <Footer />
-  </div>
+    <>
+      <div className="App">
+        <Header />
+          <Main>
+            <div>
+              <Outlet />
+            </div>
+          </Main>
+        <Footer />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+      />
+      </div>
+    </>
   )
 }
 
--- product-app/src/layouts/Header/index.jsx
+++ product-app/src/layouts/Header/index.jsx
@@ -3,14 +3,16 @@ import styles from './styles.module.css'
 import telephone from '../../assets/telephone-handle-silhouette_icon-icons.com_73380.svg'
 import cart from '../../assets/basket_96252.svg'
 import profile from '../../assets/4092564-about-mobile-ui-profile-ui-user-website_114033.svg'
-import { useState } from 'react'
 import { Modal } from '../Modal'
 import { FormRegistration } from '../FormRegistration'
 import { FormAuthorization } from '../FormAuthorization'
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
 
 
 export function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [auth, setAuth] = useState(true)
   const navigate = useNavigate()
 
   const openModal = () => {
@@ -21,7 +23,12 @@ export function Header() {
     setIsModalOpen(false)
   }
 
-  const user = localStorage.getItem('user')
+  const remove = () => {
+    localStorage.clear()
+    setAuth((prev) => !prev)
+  }
+
+  const userToken = localStorage.getItem('USER_KEY')
 
   return (
     <>
@@ -40,24 +47,25 @@ export function Header() {
           +7(999)-99-99-99
         </div>
         <div className={styles.containerLogo}>Логотип</div>
-        {/* {user ? ( */}
+        {userToken? ( 
           <div className={styles.containerProfile}>
           <img src={profile} onClick={() => navigate('/user')} className={styles.img} alt='Профиль' />
           <img src={cart} className={styles.img} alt='Корзина' />
+          <button className={styles.btn} onClick={() => remove()} type="button">Выйти</button>
         </div>
-        {/* ) : ( */}
-          <button type="button" className={styles.btn} onClick={() => openModal()}>Войти</button>
-        {/* )} */}
+        ) : (
+          <button type="button" className={styles.btn} onClick={() => openModal()}>
+            {auth ? "Войти" : "Зарегистрироваться"}
+          </button>
+        )}
         </div>
       </div>
       <Modal isOpen={isModalOpen} closeHendler={closeModal}>
-        {user ?
-          <FormAuthorization closeModal={closeModal} />
+        {auth ?
+          <FormAuthorization closeModal={closeModal} change={setAuth} />
         :
-          <FormRegistration closeModal={closeModal} />
+          <FormRegistration closeModal={closeModal} change={setAuth} />
         }
-        {/* <FormRegistration closeModal={closeModal} /> */}
-        {/* <FormAuthorization closeModal={closeModal} /> */}
       </Modal>
     </>
   )
