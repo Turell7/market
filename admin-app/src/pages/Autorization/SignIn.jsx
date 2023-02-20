804253b7a0bc092296d0cc9b35e09f72c81acd3a
Н С <nikolai3578@gmail.com>
2023-02-20 19:42:41 +0300

    add signUp & signIn

---
+++ admin-app/src/pages/Autorization/SignIn.jsx
@@ -0,0 +1,3 @@
+export function SignIn() {
+  return <div>SignIn</div>
+}
--- admin-app/src/pages/Autorization/index.jsx
+++ admin-app/src/pages/Autorization/index.jsx
@@ -1,5 +1,15 @@
+import { Container, CssBaseline } from '@mui/material'
+import { useState } from 'react'
+import { SignIn } from './SignIn'
+import { SignUp } from './SignUp'
+
 export function Authorization() {
+  const [auth, setAuth] = useState(true)
   return (
-    <div>Authorization</div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      { auth ? <SignIn change={setAuth} />
+        : <SignUp change={setAuth} />}
+    </Container>
   )
 }
