b5c4c84e2a9970a556fe6423d9c8e37ea6c94b97
Irina Gorshkova <komlyanovna174@mail.ru>
2023-02-21 20:31:21 +0500

    Added request category

--- product-app/src/layouts/TopNav/index.jsx
+++ product-app/src/layouts/TopNav/index.jsx
@@ -1,14 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { api } from '../../../Api'
 import styles from './styles.module.scss'
 
+export const CATEGORY = ['CATEGORY']
+
 export function TopNav() {
+  const { data } = useQuery({
+    queryKey: CATEGORY,
+    queryFn: () => api.getProductsCategory(),
+  })
+
   return (
     <div className={styles.containerCategoty}>
       <div className={styles.container}>
-        <div>Бренды</div>
-        <div>Женский</div>
-        <div>Мужской</div>
-        <div>Унисекс</div>
-        <div>Контакты</div>
+        {data.data.map((el) => (
+          <div>{el.name}</div>
+        ))}
       </div>
     </div>
   )
