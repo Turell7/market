160feabc5725ff170dbcb5e1a284195a4b3faffd
Irina Gorshkova <komlyanovna174@mail.ru>
2023-02-21 10:35:37 +0500

    cherry-pick product-app redux & favorites-2

--- product-app/src/redux/slices/userSlices/store.js
+++ product-app/src/redux/slices/userSlices/store.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/no-named-as-default */
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import cartSlice from '../cartSlice/cartSlice'
+import favorteSlice from '../favoriteSlice/favorteSlice'
 import userSlices from './userSlice/userSlices'
 
 const USER_KEY = ['USER_KEY']
+const FAVORITE_KEY = ['FAVORITE_KEY']
+const CART_KEY = ['CART_KEY']
 
 const middleware = getDefaultMiddleware({
   immutableCheck: false,
@@ -12,12 +17,16 @@ const middleware = getDefaultMiddleware({
 export const store = configureStore({
   reducer: {
     user: userSlices,
+    favorites: favorteSlice,
+    cart: cartSlice,
   },
   middleware,
 })
 
 store.subscribe(() => {
   const storeState = store.getState()
-  const { user } = storeState
+  const { user, favorite, cart } = storeState
   localStorage.setItem(USER_KEY, JSON.stringify(user))
+  localStorage.setItem(FAVORITE_KEY, JSON.stringify(favorite))
+  localStorage.setItem(CART_KEY, JSON.stringify(cart))
 })
