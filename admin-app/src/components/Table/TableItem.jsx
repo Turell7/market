3fb32a48b8436df3af06420a9aab45254fe5dd5a
Н С <nikolai3578@gmail.com>
2023-02-26 00:44:56 +0300

    refactoring table

--- admin-app/src/components/Table/TableItem.jsx
+++ admin-app/src/components/Table/TableItem.jsx
@@ -2,18 +2,14 @@
 // import { adminApi } from '../../Api'
 // import { PRODUCTS_QUERY_KEY } from '../../tools/queryKeys'
 
-export function TableItem({ item, titlsTable }) {
-  // const queryClient = useQueryClient()
-
-  // const onSuccessHandler = () => {
-  //   queryClient.invalidateQueries(PRODUCTS_QUERY_KEY.concat(item))
+export function TableItem({
+  item, titlsTable, deliteItem,
+}) {
+  // const clickHandler = () => {
+  //   setDeliteItem(item.id)
+  //   // onDelete
   // }
-
-  // const { mutate } = useMutation({
-  //   mutationFn: () => adminApi.deleteProduct(item.id),
-  //   onSuccess: onSuccessHandler,
-  // })
-  const tableRow = () => titlsTable.map((e) => {
+  const tableRow = titlsTable.map((e) => {
     if ((Array.isArray(item[e]))) {
       return <td key={e}>Array</td>
     }
@@ -42,9 +38,9 @@ export function TableItem({ item, titlsTable }) {
           <input type="checkbox" className="checkbox" />
         </div>
       </th>
-      {tableRow()}
+      {tableRow}
       <th>
-        <button type="button" className="btn btn-error btn-circle btn-sm bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+        <button onChange={deliteItem} type="button" className="btn btn-error btn-circle btn-sm bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
             <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
